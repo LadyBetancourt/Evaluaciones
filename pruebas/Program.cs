a5b1e8c43c04c7ea82f9efc7f0817b31502f5b8d
LadyBetancourt <lady_betancourt24@unikino.edu.mx>
2024-10-11 15:30:41 -0700

    Agregar archivos de proyecto.

---
+++ pruebas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Ingresa la calificación: ");
+        int calificacion = int.Parse(Console.ReadLine());
+
+        
+        if (calificacion >= 70)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"La calificación es de {calificacion}, aprobado");
+        }
+        else if (calificacion > 50)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"La calificación es de {calificacion}, reprobado con derecho a regularizar");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"La calificación es de {calificacion}, reprobado debe volver a cursar");
+        }
+
+        
+        Console.ResetColor();
+    }
+}
+
